77feda9b216fc6d7a23f1f9a423f8e0459476995
Precious_chukwu <preciousnwachukwu891@gmail.com>
2024-03-04 00:56:35 +0100

    thirdOfMarchUpdate

---
+++ index.js
@@ -0,0 +1,10 @@
+// let projectSection = document.getElementById("projectSection")
+// let projectLink = document.getElementById("projectLink")
+
+// let scroll = function(projectSection){
+//     if(projectSection){
+//         projectSection.scrollIntoView({behaviour: `smooth`})
+//     }
+// }
+
+// projectLink.addEventListener(`click`, scroll(projectSection))
